a31e52e39719ce7f487b43a22f1b78d167758f7a
wutong <wutong@gmail.com>
2021-10-06 16:37:38 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/db/Province.java
@@ -0,0 +1,38 @@
+package db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * author : 梧桐
+ * date : 2021/10/616:21
+ * package : db
+ */
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
